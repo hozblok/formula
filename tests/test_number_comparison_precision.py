fd2823c1571806c9a34e8bee09357bd44ed62e75
Ivan (don_vanchos) Ergunov <hozblok@gmail.com>
2026-05-16 22:42:27 +0200

    Update test_number_comparison_precision.py

--- tests/test_number_comparison_precision.py
+++ tests/test_number_comparison_precision.py
@@ -5,21 +5,21 @@ from formula import Number
 
 
 def test_number_gt_at_high_precision():
-    a = Number("1." + "0" * 50 + "1", precision=256)
+    a = Number("1." + "0" * 250 + "1", precision=256)
     b = Number("1.0", precision=256)
     assert a > b
     assert not (b > a)
 
 
 def test_number_lt_at_high_precision():
-    a = Number("1." + "0" * 50 + "1", precision=256)
+    a = Number("1." + "0" * 250 + "1", precision=256)
     b = Number("1.0", precision=256)
     assert b < a
     assert not (a < b)
 
 
 def test_number_ge_le_at_high_precision():
-    a = Number("1." + "0" * 50 + "1", precision=256)
+    a = Number("1." + "0" * 250 + "1", precision=256)
     b = Number("1.0", precision=256)
     assert a >= b
     assert b <= a
