605b670619a0c9c87e3c23c730c6857da2b7b187
Ivan Ergunov <hozblok@gmail.com>
2026-07-05 23:04:17 +0300

    fix

--- src/formula/capsysred/fresnel.py
+++ src/formula/capsysred/fresnel.py
@@ -15,15 +15,13 @@ class FresnelAmplitude:
         two = Number("2", p)
         self.d2 = material.delta(energy_kev, p) * two
         self.b2i = material.beta(energy_kev, p) * two * Number("i", p)
-        self._half = Number("0.5", p)
-        self._one = Number("1", p)
 
     def __call__(self, sin_theta: Number) -> Number:
-        root = (sin_theta * sin_theta - self.d2 - self.b2i) ** self._half
+        root = (sin_theta * sin_theta - self.d2 - self.b2i).sqrt()
         return (sin_theta - root) / (sin_theta + root)
 
     def product(self, sins: list[Number]) -> Number:
-        amp = self._one
+        amp = Number("1", self.d2.precision)
         for s in sins:
             amp = amp * self(s)
         return amp
